bc26929424e7e621e19aedd5d1f01db8eb45e0c6
ItsDaSpecialK <ItsDaSpecialK@users.noreply.github.com>
2017-11-13 20:50:23 +0100

    The bounds checking also kills the velocity now.

--- Simulations/MassSpringSystemSimulator.cpp
+++ Simulations/MassSpringSystemSimulator.cpp
@@ -296,20 +296,29 @@ void MassSpringSystemSimulator::applyExternalForce(Vec3 force)
 
 void MassSpringSystemSimulator::enforceBounds() {
 	for (Point& point : points) {
-		if (point.position.x < -BOX_DIMENSION)
+		if (point.position.x < -BOX_DIMENSION) {
 			point.position.x = -BOX_DIMENSION;
-		else if (point.position.x > BOX_DIMENSION)
+			point.velocity.x = 0;
+		} else if (point.position.x > BOX_DIMENSION) {
 			point.position.x = BOX_DIMENSION;
+			point.velocity.x = 0;
+		}
 
-		if (point.position.y < -BOX_DIMENSION)
+		if (point.position.y < -BOX_DIMENSION) {
 			point.position.y = -BOX_DIMENSION;
-		else if (point.position.y > BOX_DIMENSION)
+			point.velocity.y = 0;
+		} else if (point.position.y > BOX_DIMENSION) {
 			point.position.y = BOX_DIMENSION;
+			point.velocity.y = 0;
+		}
 
-		if (point.position.z < -BOX_DIMENSION)
+		if (point.position.z < -BOX_DIMENSION) {
 			point.position.z = -BOX_DIMENSION;
-		else if (point.position.z > BOX_DIMENSION)
+			point.velocity.z = 0;
+		} else if (point.position.z > BOX_DIMENSION) {
 			point.position.z = BOX_DIMENSION;
+			point.velocity.z = 0;
+		}
 	}
 }
 
